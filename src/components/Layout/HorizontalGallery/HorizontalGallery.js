eb39af21de5c2d566ed088f2557bbcc893c9f825
Sang Tran Phuc <sang.tp.11296@gmail.com>
2022-09-28 23:48:14 +0700

    new

--- src/components/Layout/HorizontalGallery/HorizontalGallery.js
+++ src/components/Layout/HorizontalGallery/HorizontalGallery.js
@@ -3,10 +3,10 @@ import styles from './HorizontalGallery.module.css'
 import styled from 'styled-components'
 
 const ImgCapt = styled.div`
-  position: absolute;
-  text-transform: uppercase;
-  transform: ${props => props.isRect?'':'rotate(-90deg) translate(-50%, -100%)'};
-  bottom: 0;
+  // position: absolute;
+  // text-transform: uppercase;
+  // transform: ${props => props.isRect?'':'rotate(-90deg) translate(-50%, -100%)'};
+  // bottom: 0;
 `
 const images = [
   {
@@ -33,9 +33,8 @@ function HorizontalGallery() {
   var [allImageCover, setAllImageCover] = useState([])
   var [allImageDesc, setAllImageDesc] = useState([])
   var [allImageCapt, setAllImageCapt] = useState([])
-  var [isRect, setIsRect] = useState(null)
-  var [posX, setPosX] = useState(null)
-  var [posY, setPosY] = useState(null)
+  var [posRight, setPosRight] = useState([])
+  var [posBottom, setPosBottom] = useState([])
 
   useLayoutEffect(() => {
     console.log('layout')
@@ -46,153 +45,134 @@ function HorizontalGallery() {
     allImageCover = [...new Set(allImageCover)]
     allImageDesc = [...new Set(allImageDesc)]
     allImageCapt = [...new Set(allImageCapt)]
+    console.log(allImageCover,'allImageCover in useEff')
+    console.log('positionUpdate')
+    positionUpdate();
     
-    // Compare height and width
-    var width;
-    var height;
-    console.log(allImageCover)
-    
-    for (let x = 0; x < allImageCover.length; x++){
-      console.log(allImageCover[x])
-      width = allImageCover[x].getBoundingClientRect().width
-      height = allImageCover[x].getBoundingClientRect().height
-      // if(width>height){
-      //   // return isRect to postion author, title and description
-      //   setIsRect(items => [...items,true])
-      //   // Store used area for next positioning
-      //   if(allImageCover[x].getBoundingClientRect().right > allImageDesc[x].getBoundingClientRect().right){
-      //     setPosX(allImageCover[x].getBoundingClientRect().right);
-      //   }
-      //   else setPosX(allImageDesc[x].getBoundingClientRect().right);
-
-      //   setPosY(posY + allImageCapt[x].getBoundingClientRect().bottom);
-      // }
-      // else {
-      //   // return isRect to postion author, title and description
-      //   setIsRect(items => [...items,false])
-      //   // Store used area for next positioning
-      //   if(allImageCover[x].getBoundingClientRect().right > allImageDesc[x].getBoundingClientRect().right){
-      //     setPosX(allImageCover[x].getBoundingClientRect().right);
-      //   }
-      //   else setPosX(allImageDesc[x].getBoundingClientRect().right);
-
-      //   setPosY(posY + allImageCapt[x].getBoundingClientRect().bottom);
-      // }
-    }
   }, [allImageCover])
 
-  // Styling the post depending on isRect state
-  const wrapperStyle = {
-    top: posX + 'px' ,
-    left: posY + 'px'
+  // Function update next position for next post
+  function positionUpdate(){
+    for (let x = 0; x < allImageCover.length; x++){
+      console.log(allImageCover[x],'in function')
+      const offset = 1311;
+      let nPosRight = 0;
+      let nPosBottom = 0;
+      
+      if (x = 0){
+        // Store used area for next positioning with rectangle posts
+        if(allImageCover[x].getBoundingClientRect().width > allImageCover[x].getBoundingClientRect().height){
+          if(allImageCover[x].getBoundingClientRect().right > allImageDesc[x].getBoundingClientRect().right){
+            setPosRight(allPosRight=>[...allPosRight, allImageCover[x].getBoundingClientRect().right])
+            setPosBottom(allPosY=>[...allPosY,allImageCapt[x].getBoundingClientRect().bottom]);
+          }
+          else {
+            setPosRight(allPosRight=>[...allPosRight, allImageDesc[x].getBoundingClientRect().right]);
+            setPosBottom(allPosY=>[...allPosY,allImageCapt[x].getBoundingClientRect().bottom]);
+          }
+        }
+        else {
+          setPosRight(allPosR=>[...allPosR,allImageCover[x].getBoundingClientRect().right]);
+          setPosBottom(allPosY=>[...allPosY,allImageDesc[x].getBoundingClientRect().bottom]);
+        }
+      }
+
+      if(x > 0){
+         // Store used area for next positioning with rectangle posts
+        if(allImageCover[x].getBoundingClientRect().width > allImageCover[x].getBoundingClientRect().height){
+          // Generate random position Left
+          nPosLeft = Math.random() * ((allImageCover[x].getBoundingClientRect().right + 10) - allImageCover[x].getBoundingClientRect().right) + allImageCover[x].getBoundingClientRect().right;
+          setPosLeft(allPosX=>[...allPosX,nPosLeft]);
+        
+          // Generate random position top
+          const remainTop = offset - allImageCapt[x].getBoundingClientRect().bottom - 10
+          if (remainTop>= allImageCapt[x+1])
+          setPosTop(allPosY=>[...allPosY,allImageCapt[x].getBoundingClientRect().bottom]);
+      
+          else setPosLeft(allPosX=>[...allPosX,allImageDesc[x].getBoundingClientRect().right]);
+            // Store used area for next positioning with other posts
+        }
+      }
+      
+    }
   }
-  const captStyle = {}
-  
-  const descStyle = {}
-  useEffect(() => {
-    if (isRect) {
-      console.log(isRect)
-      
-      // Random position for caption
-      if (Math.random() < 0.5){
-        captStyle.map((att)=>({
-          ...att,
-          position: 'absolute',
-          textTransform: 'uppercase',
-          bottom: '0px',
-          transform : 'rotate(-90deg) translate(-50%, -100%)',
-          right : '0px',
-          textAlign : 'right'
-        }))
-      }
-      else {
-        captStyle.map((att)=>({
-          ...att,
-          position: 'absolute',
-          textTransform: 'uppercase',
-          bottom: '0px',
-          transform : 'rotate(-90deg) translate(-50%, -100%)',
-          transform : 'translateY(100%)',
-          left : '0px',
-          textAlign : 'left'
-        }))
-      }
-
-      // Random position for description
-      if (Math.random() < 0.5){
-        descStyle.map((att)=>({
-          ...att,
-          right : '0px',
-          transform : 'translateX(100%)',
-          textAlign :'left',
-          paddingLeft : '10px'
-        }))
-      }
-      else {
-        descStyle.map((att)=>({
-          ...att,
-          left : '0px',
-          transform : 'translateX(-100%)',
-          textAlign :'right',
-          paddingRight : '10px'
-        }))
-      }
-    }
-    else {
-      captStyle.map((att)=>({
-        ...att,
-        position: 'absolute',
-        textTransform: 'uppercase',
-        bottom: '0px',
-        transform : 'translateY(100%)',
-        right : '0px',
-        textAlign : 'right',
-        transformOrigin : 'left top',
-        paddingBottom : '5px'
-      }))
-      // Random position for description
-      if (Math.random() < 0.5){
-        descStyle.map((att)=>({
-          ...att,
-          right : '0px',
-          bottom : '0px',
-          transform : 'translateY(100%)',
-          textAlign :'right',
-          paddingTop : '6px'
-        }))
-      }
-      else {
-        descStyle.map((att)=>({
-          ...att,
-          left : '0px',
-          bottom : '0px',
-          transform : 'translateY(100%)',
-          textAlign :'left',
-          paddingTop : '6px'
-        }))
-      }
-    }
-      
-  }, [isRect])
-  
-
+  console.log(posRight,posBottom)
   return (
     <div className={styles.fullpageWrapper}>
       {/* Mapping list of Images */}
       {images.map((item,ind) => {
-        console.log(ind)
-        console.log(allImageCover[ind])
+        console.log(ind,'ind in render')
 
+        // Styling the post depending on isRect state
+        const wrapperStyle = {
+          top: posRight[ind] + 'px',
+          left: posBottom[ind] + 'px'
+        }
+        const captStyle = {}
+        
+        const descStyle = {}
         if (allImageCover[ind]){
           var width = allImageCover[ind].getBoundingClientRect().width
           var height = allImageCover[ind].getBoundingClientRect().height
           if (width>height){
-            setIsRect(true)
+            // Random position for caption
+            if (Math.random() < 0.5){
+              captStyle.position = 'absolute',
+              captStyle.textTransform = 'uppercase',
+              captStyle.bottom = '0px',
+              captStyle.transform  = 'translateY(100%)',
+              captStyle.right = '0px',
+              captStyle.textAlign = 'right'
+            }
+            else {
+              captStyle.position = 'absolute',
+              captStyle.textTransform = 'uppercase',
+              captStyle.bottom = '0px',
+              captStyle.transform  = 'translateY(100%)',
+              captStyle.left = '0px',
+              captStyle.textAlign = 'left'
+            }
+            // Random position for description
+            if (Math.random() < 0.5){
+              descStyle.right = '0px',
+              descStyle.transform = 'translateX(100%)',
+              descStyle.textAlign ='left',
+              descStyle.paddingLeft = '10px'
+            }
+            else {
+              descStyle.left = '0px',
+              descStyle.transform = 'translateX(-100%)',
+              descStyle.textAlign ='right',
+              descStyle.paddingRight = '10px'
+            }
           }
-          else setIsRect(false)
+          else {
+            captStyle.position = 'absolute',
+            captStyle.textTransform = 'uppercase',
+            captStyle.bottom = '0px',
+            // captStyle.transform = 'translateY(100%)',
+            captStyle.transform = 'rotate(-90deg) translate(-50%, -100%)',
+            captStyle.textAlign = 'right',
+            captStyle.transformOrigin = 'left top',
+            captStyle.paddingBottom = '5px'
+            
+            // Random position for description
+            if (Math.random() < 0.5){
+              descStyle.right = '0px',
+              descStyle.bottom = '0px',
+              descStyle.transform = 'translateY(100%)',
+              descStyle.textAlign ='right',
+              descStyle.paddingTop = '6px'
+            }
+            else {
+              descStyle.left = '0px',
+              descStyle.bottom = '0px',
+              descStyle.transform = 'translateY(100%)',
+              descStyle.textAlign ='left',
+              descStyle.paddingTop = '6px'
+            }
+          }
         }
-        
-        
         return(
           <div key={ind} className={styles.imgWrapper} style={wrapperStyle}>
               <div className={styles.imgContent}>

7f3cae4ea075c357181a50349b2602520c3b024e
Sang Tran Phuc <sang.tp.11296@gmail.com>
2022-10-04 23:37:34 +0700

    update

--- src/components/Layout/HorizontalGallery/HorizontalGallery.js
+++ src/components/Layout/HorizontalGallery/HorizontalGallery.js
@@ -1,13 +1,7 @@
 import React,{useEffect, useState, useLayoutEffect} from 'react'
 import styles from './HorizontalGallery.module.css'
-import styled from 'styled-components'
 
-const ImgCapt = styled.div`
-  // position: absolute;
-  // text-transform: uppercase;
-  // transform: ${props => props.isRect?'':'rotate(-90deg) translate(-50%, -100%)'};
-  // bottom: 0;
-`
+
 const images = [
   {
     img: 'https://images.unsplash.com/photo-1583144584182-1717fab24b1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80'
@@ -15,9 +9,9 @@ const images = [
   {
     img: 'https://images.unsplash.com/photo-1563124488-159c05ebb7e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80'
   },
-  // {
-  //   img: 'https://images.unsplash.com/photo-1582370930143-4c547062f8bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3772&q=80'
-  // },
+  {
+    img: 'https://images.unsplash.com/photo-1582370930143-4c547062f8bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3772&q=80'
+  },
   // {
   //   img: 'https://images.unsplash.com/photo-1589482338352-c3ab369b4b61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3872&q=80'
   // },
@@ -66,9 +60,10 @@ function HorizontalGallery() {
     var left;
     var right;
 
-    if(allPosts.image[0]){
+    if(x == 0){
       // Store a, b point of the rectangle posts
       if(allPosts.image[0].getBoundingClientRect().width>allPosts.image[0].getBoundingClientRect().height){
+        console.log(allPosts.image[0].getBoundingClientRect().right > allPosts.desc[0].getBoundingClientRect().right)
         if(allPosts.image[0].getBoundingClientRect().right > allPosts.desc[0].getBoundingClientRect().right){
             top = allPosts.image[0].getBoundingClientRect().top
             left = allPosts.desc[0].getBoundingClientRect().left
@@ -90,50 +85,57 @@ function HorizontalGallery() {
         bot = allPosts.desc[0].getBoundingClientRect().bottom
       }
       postPoss.push({top, left, bot, right})
-      console.log(postPoss)
     }
-    while(!checkValid(top, left, bot, right)){
-      // Generate position for rectangle post type
-      if(!allPosts.image[0] && allPosts.image[x]){
-        if(allPosts.image[x].getBoundingClientRect().width > allPosts.image[x].getBoundingClientRect().height){
-          if(allPosts.image[x].getBoundingClientRect().right > allPosts.desc[x].getBoundingClientRect().right){
-            top = Math.random()*(offset-allPosts.image[x].getBoundingClientRect().height)
-            left = Math.random()*(postPoss[x-1].right + 20) - allPosts.desc.getBoundingClientRect().width
-            bot = postPoss[x].top + allPosts.image[x].getBoundingClientRect().height + allPosts.capt.getBoundingClientRect().height
-            right = postPoss[x].left + allPosts.desc[x].getBoundingClientRect().width + allPosts.image.getBoundingClientRect().width
-          }
-          else{
-            top = Math.random()*(offset-allPosts.image[x].getBoundingClientRect().height)
-            left = Math.random()*(postPoss[x-1].right + 20)
-            bot = postPoss[x].top + allPosts.image[x].getBoundingClientRect().height + allPosts.capt[x].getBoundingClientRect().height
-            right = postPoss[x].left + allPosts.desc[x].getBoundingClientRect().width + allPosts.image[x].getBoundingClientRect().width
+    else{
+      while(!top && !left && !bot && !right || !checkValid(top, left, bot, right)){
+        // Generate position for rectangle post type
+        if(!allPosts.image[0] && allPosts.image[x]){
+          if(allPosts.image[x].getBoundingClientRect().width > allPosts.image[x].getBoundingClientRect().height){
+            if(allPosts.image[x].getBoundingClientRect().right > allPosts.desc[x].getBoundingClientRect().right){
+              top = Math.floor(Math.random()*(offset - allPosts.image[x].getBoundingClientRect().height - allPosts.capt[x].getBoundingClientRect().height))
+              left = Math.floor(Math.random()*(postPoss[x-1].right + 20) - allPosts.desc[x].getBoundingClientRect().width)
+              bot = top + allPosts.image[x].getBoundingClientRect().height + allPosts.capt[x].getBoundingClientRect().height
+              right = left + allPosts.desc[x].getBoundingClientRect().width + allPosts.image[x].getBoundingClientRect().width
+            }
+            else{
+              top = Math.floor(Math.random()*(offset - allPosts.image[x].getBoundingClientRect().height - allPosts.capt[x].getBoundingClientRect().height))
+              left = Math.floor(Math.random()*(postPoss[x-1].right + 20))
+              bot = top + allPosts.image[x].getBoundingClientRect().height + allPosts.capt[x].getBoundingClientRect().height
+              right = left + allPosts.desc[x].getBoundingClientRect().width + allPosts.image[x].getBoundingClientRect().width
+            }
           }
         }
+        // Generate position for other post type
+        else{
+          top = Math.floor(Math.random()*(offset - allPosts.image[x].getBoundingClientRect().height - allPosts.desc[x].getBoundingClientRect().height))
+          left = Math.floor(Math.random()*(postPoss[x-1].right + 100)) - allPosts.capt[x].getBoundingClientRect().width
+          bot = top + allPosts.image[x].getBoundingClientRect().height + allPosts.desc[x].getBoundingClientRect().height
+          right = left + allPosts.capt[x].getBoundingClientRect().width + allPosts.image[x].getBoundingClientRect().width
+        }
+        if (top && left && bot && right && checkValid(top, left, bot, right)) {
+          postPoss.push({top, left, bot, right});
+          break;
+        }
       }
-      // Generate position for other post type
-      else{
-        top = Math.random()*(offset-allPosts.image[x].getBoundingClientRect().height)
-        left = Math.random()*(postPoss[x-1].right + 20) - allPosts.capt.getBoundingClientRect().width
-        bot = postPoss[x].top + allPosts[x].image.getBoundingClientRect().height + allPosts.desc.getBoundingClientRect().height
-        right = postPoss[x].left + allPosts.capt.getBoundingClientRect().width + allPosts.image.getBoundingClientRect().width
-      }
-      if (checkValid(top, left, bot, right)) break;
     }
-    postPoss.push({top, left, bot, right});
-    
+    console.log(postPoss)
   }
   // Check if aPoint and bPoint valid
   function checkValid(t, l, b, r){
     for (let postPos of postPoss){
-      if (t > postPos.top && t < postPos.bot && l > postPos.left && l < postPos.right){
+      if (t > postPos.top && t < postPos.bot){
         return false
       }
-      else{
-        if (b > postPos.top && b < postPos.bot && r > postPos.left && r < postPos.right){
-          return false
-        }
-        else return true
+      else if(l > postPos.left && l < postPos.right){
+        return false
       }
+      else if(b > postPos.top && b < postPos.bot){
+        return false
+      }
+      else if(r > postPos.left && r < postPos.right){
+        return false
+      }
+      else return true
     }
   }
   return (
@@ -141,11 +143,12 @@ function HorizontalGallery() {
       {/* Mapping list of Images */}
       {images.map((item,ind) => {
         console.log(ind,'ind in render')
-        positionUpdate(ind)
-       // Styling the post depending on isRect state
+        console.log(allPosts.image[ind])
+        {allPosts.image[ind] ? positionUpdate(ind) : ''}
+        // Styling the post depending on isRect state
         const wrapperStyle = {
-          top: aPoint.top[ind] + 'px',
-          left: aPoint.left[ind] + 'px'
+          top: `${!allPosts.image[ind] ? 0 : postPoss[ind].top}` + 'px',
+          left: `${!allPosts.image[ind] ? 0 : postPoss[ind].left}`+ 'px'
         }
         const captStyle = {}
         

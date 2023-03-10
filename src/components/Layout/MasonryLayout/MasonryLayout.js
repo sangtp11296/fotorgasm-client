70dc0d6c1ce103cfc408e3a68bfcba46345a9135
Sang Tran Phuc <sang.tp.11296@gmail.com>
2023-03-11 00:14:31 +0700

    update

--- src/components/Layout/MasonryLayout/MasonryLayout.js
+++ src/components/Layout/MasonryLayout/MasonryLayout.js
@@ -1,47 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import styles from './MasonryLayout.module.css'
-import Masonry from 'react-masonry-css'
 import axios from 'axios'
-function MasonryLayout() {
-    const [image, setImage] = useState([])
-    const [hasMore, setHasMore] = useState(true)
-    const [page, setPage] = useState(1)
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await axios.get(
-                `https://api.unsplash.com/photos/random?count=12&page=${page}`,
-                {
-                    headers: {
-                        Authorization: `Client-ID oToAvK2epPDap7yqFnLiUzugpJ2Za7bOuPgu2Crq_QE`,
-                    }
-                }
-            );
-            if (res.data.length === 0) {
-                setHasMore(false);
-            } else {
-                setImage([...image, ...Response.data]);
-            }
-        };
-        fetchData()
-    }, [page])
-    const breakpointColumnsObj = {
-        default: 4,
-        1100: 3,
-        700: 2,
-        500: 1,
-    }
+function MasonryLayout({images}) {
+
   return (
-    <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className='my-masonry-grid'
-        columnClassName='my-masonry-grid_column'
-    >
-        {image.map((img) => (
-            <div key={img.id}>
-                <img src={img.urls.regular} alt={img.alt_description} />
-            </div>
-        ))}
-    </Masonry>
+    <div className={styles.gridContainer}>
+        {images.map((img) => {
+            if(img.width >= img.height){
+                return(
+                    <div className={`${styles.mediaType} ${styles.postBlog}`} key={img.id}>
+                        <img src={img.urls.regular} alt={img.alt_description} />
+                    </div>
+                )
+            }
+            return(
+                <div className={`${styles.standardType} ${styles.postBlog}`} key={img.id}>
+                    <img src={img.urls.regular} alt={img.alt_description} />
+                </div>
+        )})}
+    </div>
   )
 }
 
--- src/components/Layout/PostGrid/PostGrid.js
+++ src/components/Layout/PostGrid/PostGrid.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import MasonryLayout from '../MasonryLayout/MasonryLayout'
 import styles from './PostGrid.module.css'
+import axios from 'axios';
 
 function PostGrid() {
     const [click, isClicked] = useState('blog')
+    const [images, setImages] = useState([]);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+
+    const loadMoreImages = () => {
+        setPage(page + 1);
+    };
+    useEffect(() => {
+        const fetchImages = async () => {
+            const response = await axios.get(
+                `https://api.unsplash.com/photos/random?count=5&page=${page}`,
+                {
+                    headers: {
+                        Authorization: `Client-ID oToAvK2epPDap7yqFnLiUzugpJ2Za7bOuPgu2Crq_QE`,
+                    }
+                }
+            );
+            if (response.data.length === 0) {
+                setHasMore(false);
+            } else {
+                setImages(prevImages => [...prevImages, ...response.data]);
+            }
+        };
+        fetchImages()
+    }, [page])
   return (
     <>
         <div className={styles.postCatalog}>
@@ -25,9 +52,16 @@ function PostGrid() {
             </div>
         </div>
         <div className={styles.postGrid}>
-            <div className={styles.gridContainer}>
-                <MasonryLayout/>
-            </div>
+            <InfiniteScroll 
+                dataLength={images.length}
+                next={loadMoreImages}
+                hasMore={hasMore}
+                loader={<h4>Loading...</h4>}
+                style={{ overflow: 'hidden' }}
+            >
+                <MasonryLayout images={images}/>
+
+            </InfiniteScroll>
         </div>
     </>
   )

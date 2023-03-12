126447c851bc3ee2c36d562c3ae209f0307fbb75
Sang Tran Phuc <sang.tp.11296@gmail.com>
2023-03-13 00:00:37 +0700

    update

--- src/components/Layout/MasonryLayout/MasonryLayout.js
+++ src/components/Layout/MasonryLayout/MasonryLayout.js
@@ -12,6 +12,20 @@ function MasonryLayout({images}) {
                         <div className={`${styles.catPost} ${styles.bubble}`}>
                             <span>on Videos</span>
                         </div>
+                        <div className={styles.titlePost}>
+                            <h1>
+                                Gooey Text Background With SVG Filters
+                            </h1>
+                            
+                            <svg style={{visibility: 'hidden', position: 'absolute'}} xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                <defs>
+                                    <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />    
+                                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                                        <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+                                    </filter>
+                                </defs>
+                            </svg>
+                        </div>
                         <img src={img.urls.regular} alt={img.alt_description} />
                     </div>
                 )
